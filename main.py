7e05770a2d8640fad4baa61e82b6baba6d8d703d
SandaL <smartyalexpro@gmail.com>
2023-11-09 19:21:19 +0400

    Print db data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from typing import Union
 
+import socket
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -13,3 +14,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    uvicorn.run("main:app", host=ip_address, port=8000, log_level="info")
